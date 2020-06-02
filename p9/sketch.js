d60dd9494ab31a31bff5f2616cab75fccd3e9823
stella1208 <62825946+stella1208@users.noreply.github.com>
2020-06-02 12:59:21 +0900

    Add files via upload

---
+++ p9/sketch.js
@@ -0,0 +1,90 @@
+
+var num = 2000;
+var noiseScale=500, noiseStrength=1;
+var particles = [num];
+var x;
+var y;
+var z;
+var radius;
+var n=20;
+var a=100;
+var angle;
+var money;
+var sv;
+function setup() {
+  createCanvas(windowWidth, windowHeight,WEBGL);
+ 
+  noStroke();
+  x=0
+  y=windowHeight/2;
+  z=0;
+  radius=100;
+ 
+  for (let i=0; i<num; i++) {
+    
+    var loc = createVector(random(width*1.2), random(height), 2);
+    var angle = 0; 
+    var dir = createVector(cos(angle), sin(angle));
+    var speed = random(0.5,2);
+    particles[i]= new Particle(loc, dir, speed);
+  }
+ 
+
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+function draw() {
+  background(0);
+  lights();
+  translate(-windowWidth/2,-windowHeight/2);
+  fill(0, 10);
+  noStroke();
+  rect(0, 0, width, height);
+  
+  for (let i=0; i<particles.length; i++) {
+    particles[i].run();
+  }
+  
+}
+
+class Particle{
+  constructor(_loc,_dir,_speed){
+    this.loc = _loc;
+    this.dir = _dir;
+    this.speed = _speed;
+  
+  	// var col;
+  }
+  run() {
+    this.move();
+    this.checkEdges();
+    this.update();
+  }
+  move(){
+    let angle=noise(this.loc.x/noiseScale, this.loc.y/noiseScale, frameCount/noiseScale)*TWO_PI*noiseStrength; //0-2PI
+    this.dir.x = cos(angle);
+    this.dir.y = sin(angle);
+    var vel = this.dir.copy();
+    var d =1;  //direction change 
+    
+    vel.mult(this.speed*d); //vel = vel * (speed*d)
+    this.loc.add(vel); //loc = loc + vel
+
+  }
+  checkEdges(){
+    //float distance = dist(width/2, height/2, loc.x, loc.y);
+    //if (distance>150) {
+    if (this.loc.x<0 || this.loc.x>width || this.loc.y<0 || this.loc.y>height) {    
+      this.loc.x = random(width*1.2);
+      this.loc.y = random(height);
+    }
+  }
+  update(){
+  
+    fill(255);
+    ellipse(this.loc.x, this.loc.y, this.loc.z);
+  }
+}
+
